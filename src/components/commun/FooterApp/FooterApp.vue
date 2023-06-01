1ccf4f969a4b6bcfebbb271d8e9c541cc1635f49
Olivier Daigneault <oli1999@hotmail.ca>
2023-06-01 12:34:44 -0400

    feature-footer-placeholder

--- src/components/commun/FooterApp/FooterApp.vue
+++ src/components/commun/FooterApp/FooterApp.vue
@@ -8,6 +8,35 @@
 
         <h3>FOOTER</h3>
 
+        <div class="wrapInfo">
+
+            <div class="entreprise">
+                <div class="wrapLogo">
+                    <img src="../../../assets/img/png/no-img.png" alt="logo-pemana">
+                </div>
+            </div>
+
+            <div class="contact">
+                <h3>Contact</h3>
+                <ul>
+                    <li>info@jeanfrancoisdube.ca</li>
+                    <li>(514) 880-3770</li>
+                    <li>168, rue de la Charente, Saint-Jérôme, Québec, Canada</li>
+                </ul>
+            </div>
+
+            <div class="links">
+                <nav>
+                    <ul>
+                        <li>PAGE QUAI MARINA</li>
+                        <li>PAGE LOGEMENT TREMBLANT</li>
+                        <li>PAGE LOGEMENT ST-JÉRÔME</li>
+                    </ul>
+                </nav>
+            </div>
+
+        </div>
+
     </div>
 
 </template>
