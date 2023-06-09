581849b85c0387525df89df563ee9f2bf08cf2b1
Olivier Daigneault <oli1999@hotmail.ca>
2023-06-09 14:07:17 -0400

    feature-accueil-component

---
+++ src/components/AccueilSite/AccueilSite.vue
@@ -0,0 +1,24 @@
+<script setup>
+
+</script>
+
+<template>
+    <!-- CHANGER LA CLASS DU DIV ↴ -->
+    <div class="accueil-site">
+
+        <div class="wrapInfo">
+            <h1>PEMENA</h1>
+            <h3>Location de quai et de logement</h3>
+        </div>
+
+        <div class="wrapImg">
+            <img src="../assets/img/png/no-img.png" alt="logo-pemana">
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
