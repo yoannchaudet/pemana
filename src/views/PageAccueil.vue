eea85438fb4669f6103098bf4abad551fc95f145
Olivier Daigneault <oli1999@hotmail.ca>
2023-06-09 13:33:25 -0400

    feature-header-sidebarStyleBase

--- src/views/PageAccueil.vue
+++ src/views/PageAccueil.vue
@@ -1,12 +1,21 @@
 <script setup>
-
+    import FooterApp from '@/components/commun/FooterApp/FooterApp.vue'
 </script>
 
 <template>
     
     <div class="accueil">
 
-        <h2>PAGE ACCUEIL</h2>
+        <div class="wrapInfo">
+            <h1>PEMENA</h1>
+            <h3>Location de quai et de logement</h3>
+        </div>
+
+        <div class="wrapImg">
+            <img src="../assets/img/png/no-img.png" alt="logo-pemana">
+        </div>
+
+        <FooterApp />
 
     </div>
 
