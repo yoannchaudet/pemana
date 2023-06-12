c13151c46b7d9918867a4fd0e33ec10f4f2ccf40
Olivier Daigneault <oli1999@hotmail.ca>
2023-06-12 11:31:37 -0400

    feature-lacSup-placeholder

---
+++ src/components/AffichageData/AffichageLacSup.vue
@@ -0,0 +1,25 @@
+<script setup>
+    import VignetteLogement from '@/components/AffichageData/VignetteLogement.vue'
+    
+    import saintJeData from '@/assets/json/saintJe.json'
+</script>
+
+<template>
+    
+    <div class="affichage-lacSup">
+
+        <h1>Logements au Lac-Supérieur</h1>
+
+        <div class="liste-vignette-logement" v-if="saintJeData">
+            <VignetteLogement v-for="logement in saintJeData" :key="logement.id"
+                :objLogement="logement"
+            />
+        </div>
+
+    </div>
+
+</template>
+
+<style scoped>
+
+</style>
