83dd15ddf57847324a91c652a0619d276b1ccb58
Olivier Daigneault <oli1999@hotmail.ca>
2023-06-12 12:03:35 -0400

    fix-lacSup-database

--- src/components/AffichageData/VignetteLogement.vue
+++ src/components/AffichageData/VignetteLogement.vue
@@ -16,12 +16,14 @@
 
         <div class="wrapInfo">
             <h2 v-text="props.objLogement.name"></h2>
+            <p v-text="props.objLogement.type"></p>
             <p v-text="props.objLogement.adresse"></p>
             <p v-text="props.objLogement.description"></p>
         </div>
 
         <div class="wrapPrix">
             <p v-text="props.objLogement.prix"></p>
+            <p>par mois</p>
         </div>
 
     </div>
