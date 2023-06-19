b63aca741dc9cbaf60700f78c13da36f8e6b8348
Olivier Daigneault <oli1999@hotmail.ca>
2023-06-19 13:33:27 -0400

    test-directiveDist

--- vite.config.js
+++ vite.config.js
@@ -3,9 +3,19 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
+import { resolve } from 'path';
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/pemana',
+  base: '/pemana/',
+
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'src/*'),
+      }
+    }
+  },
 
   plugins: [vue()],
   resolve: {
