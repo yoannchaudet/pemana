f85d13ef0335d13272e58282a932a8b633d8afcd
Olivier Daigneault <oli1999@hotmail.ca>
2023-06-19 14:30:45 -0400

    attemp-static-deploy

--- vite.config.js
+++ vite.config.js
@@ -3,19 +3,9 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
-import { resolve } from 'path';
-
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/pemana/',
-
-  build: {
-    rollupOptions: {
-      input: {
-        main: resolve(__dirname, 'src/*'),
-      }
-    }
-  },
 
   plugins: [vue()],
   resolve: {
