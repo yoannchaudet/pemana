503ef6d72c01f3ecdffde7bdfc2c20077ef43341
Olivier Daigneault <oli1999@hotmail.ca>
2023-06-22 11:50:13 -0400

    feature-accueilTop-styleBase

--- src/components/AccueilSite/AccueilSite.vue
+++ src/components/AccueilSite/AccueilSite.vue
@@ -8,9 +8,19 @@
     
     <div class="accueil-site">
 
-        <div class="wrapInfo">
-            <h1>Pemana Inc.</h1>
-            <h3>Location de quai et de logement</h3>
+        <div class="accueil-top">
+            <div class="wrapImg">
+                <img src="./../../assets/img/jpg/lacSup/69-71/69-71_00.jpg" alt="lac-sup-principal">
+            </div>
+            <div class="wrapInfo">
+                <div class="info-container">
+                    <h1>Pemana Inc.</h1>
+                    <h3>Location de quai et de logement</h3>
+                    <div class="wrapImg">
+                        <img src="./../../assets/img/svg/logo_pemana_light.svg" alt="logo-light-full">
+                    </div>
+                </div>
+            </div>
         </div>
 
         <div class="listeVignetteCat" v-if="categorieData">
