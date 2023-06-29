3d43414d1fea0c03221ce441c52ed853c26eb5e2
Kebrouh <oli1999@hotmail.ca>
2023-06-29 11:17:32 -0400

    feature-marina-miseEnPageBase

--- src/components/AffichageData/AffichageMarina.vue
+++ src/components/AffichageData/AffichageMarina.vue
@@ -8,14 +8,21 @@
 
         <div class="marina-top">
             <div class="titre">
-                <h1>Marina Zool bar &amp; gîte</h1>
+                <h1>Marina</h1>
+                <h2>Zool bar &amp; gîte</h2>
+                <div class="wrapImg">
+                    <img src="@/assets/img/svg/logo_pemana_light_plein.svg" alt="">
+                </div>
             </div>
             <div class="galerie">
                 <div class="wrapImg">
-                    <img src="./../../assets/img/png/no-img.png" alt="">
+                    <img src="@/assets/img/png/no-img.png" alt="">
                 </div>
                 <div class="wrapImg">
-                    <img src="./../../assets/img/png/no-img.png" alt="">
+                    <img src="@/assets/img/png/no-img.png" alt="">
+                </div>
+                <div class="wrapLogo">
+                    <img src="@/assets/img/svg/logo_marina.svg" alt="">
                 </div>
             </div>
         </div>
@@ -35,19 +42,25 @@
                     <li>service offert</li>
                 </ul>
             </div>
+            <div class="adresse">
+                <p>692 Chem. des Outaouais, Pointe-Fortune</p>
+                <p>QC J0P 1N0</p>
+                <p>(450) 430-3330</p>
+            </div>
             <div class="wrapMap">
                 <div class="wrapImg">
-                    <img src="./../../assets/img/png/no-img.png" alt="">
+                    <img src="@/assets/img/svg/map_location_zoom.svg" alt="">
                 </div>
             </div>
         </div>
 
         <div class="marina-logement">
             <div class="wrapImg">
-                <img src="./../../assets/img/png/no-img.png" alt="">
+                <img src="@/assets/img/jpg/marina/marina_3d_1000x.jpg" alt="">
             </div>
-            <div class="warpInfo">
-                <p>The Tragedy of Darth Plagueis the Wise" was a "Sith legend" created by Darth Sidious, the Sith apprentice of the Muun Dark Lord of the Sith Darth Plagueis. The tragedy told of how a "so powerful" Sith lost all of his power, although he could save others from death, however, not himself.</p>
+            <div class="wrapInfo">
+                <h3>Projet de construction de logements près de la marina</h3>
+                <p>à venir</p>
             </div>
         </div>
 
