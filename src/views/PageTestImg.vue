1822677e20487545bfe8500a7f5574518bb76bfb
Kebrouh <oli1999@hotmail.ca>
2023-07-03 13:19:50 -0400

    fix-try3-gpt4

--- src/views/PageTestImg.vue
+++ src/views/PageTestImg.vue
@@ -6,7 +6,7 @@
   <div class="test-img">
 
     <img src="@/assets/img/jpg/lacSup/69-71/69-71_00.jpg" alt="">
-    <!-- babagaboush plesae work-->
+    <!-- babagaboush plesae work work-->
 
   </div>
 </template>
